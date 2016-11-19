23c90f3dafa2ec5f7d1537a1a26cf44f9959813e
Hernan Cano <jhernancanom@gmail.com>
2016-11-19 18:55:58 -0500

    Fixing paths for windows 7

--- lide.bat
+++ lide.bat
@@ -1,3 +1,3 @@
 @echo off
 
-lua -e "package.path = package.path ..';'..os.getenv 'LIDE_PATH' ..'/?.lua'; require 'lide.core.init'" %LIDE_PATH%\lide.lua %1 %2 %3
+lua -e "assert(os.getenv 'LIDE_PATH', 'Declare la variable de entorno LIDE_PATH'); package.path = os.getenv 'LIDE_PATH' ..'\\?.lua'; require 'lide.core.init'" %LIDE_PATH%\lide.lua %1 %2 %3
