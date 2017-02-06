dfe63cd943b6049fbc9e03c2b1c4c633c9851c9c
dcanoh <thdkano@gmail.com>
2017-02-05 22:14:29 -0500

    Add submodule libraries/lide

--- libraries/lide/tests/test_runtime_folders.lua
+++
@@ -1,6 +0,0 @@
-assert(app, 'test runtime: "app" value doesn\'t exists.')
-assert(app.sourcefolder , 'test runtime folders: value app.sourcefolder doesn\'t exists.')
-assert(app.workingfolder, 'test runtime folders: value app.workingfolder doesn\'t exists.')
-
-print (('Source folder: %s'):format(app.sourcefolder))
-print (('Working folder: %s'):format(app.workingfolder))
