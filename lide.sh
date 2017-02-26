6e7c3ef8935ac10be212985085cc4957f3f8c00f
dcanoh <thdkano@gmail.com>
2017-02-26 17:11:53 -0500

    Writing tests
    Installing Lua

--- lide.sh
+++ lide.sh
@@ -3,4 +3,4 @@
 #LUA_BIN='/datos/Proyectos/lide/commandline'
 
 #$LUA_BIN/lua514 -l lide.init $LUA_BIN/lide.lua $1 $2 $3
-lua5.1 -e 'package.path = package.path ..";"..os.getenv "LIDE_PATH" .."/?.lua" require "lide.core.init" ' $LIDE_PATH/lide.lua $1 $2 $3 $4 $5 $6 $7
+lua -e 'package.path = package.path ..";"..os.getenv "LIDE_PATH" .."/?.lua" require "lide.core.init" ' $LIDE_PATH/lide.lua $1 $2 $3 $4 $5 $6 $7
