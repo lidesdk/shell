64e1405a837cb0b31a1db17c57f31f0f481b7ad0
dcanoh <thdkano@gmail.com>
2017-03-20 03:33:30 -0500

    Updating tests

--- lide.sh
+++ lide.sh
@@ -1,7 +1,3 @@
 #!/bin/sh
-
-#LUA_BIN='/datos/Proyectos/lide/commandline'
-
-#$LUA_BIN/lua514 -l lide.init $LUA_BIN/lide.lua $1 $2 $3
 #lua -e 'package.path = (package.path or '') ..";"..(os.getenv "LIDE_PATH" or '') .."/?.lua" require "lide.core.init" ' $LIDE_PATH/lide.lua $1 $2 $3 $4 $5 $6 $7
 lua $LIDE_PATH/lide.lua $1 $2 $3 $4 $5 $6 $7
