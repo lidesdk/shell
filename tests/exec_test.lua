9e941efab63cc89320e353a61f495b7443403259
dcanoh <thdkano@gmail.com>
2017-04-22 23:49:18 -0500

    Updated tests
    Test: lide search <package>

--- tests/exec_test.lua
+++ tests/exec_test.lua
@@ -1,3 +1,10 @@
 io.stdout : write '[lide commandline] execution tests: '
+
 assert(io.popen 'lide.bat --test' :read '*a' == '[lide test] all ok.')
-io.stdout : write '[OK]\n'
+
+-- Test if luasql is on stable repo with search:
+searchline = io.popen 'lide.bat search luasql' :read '*l' 
+assert( searchline:sub (1, searchline : find '/' ) == 'stable/')
+
+
+io.stdout : write '[OK]'
