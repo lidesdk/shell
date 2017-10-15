fad36778cd342d158229e12778ff9af9b3dbb24d
Hernan Dario Cano <dcanohdev@gmail.com>
2017-10-15 06:27:48 -0500

    Update linux package tests

--- tests/[linux]commandline_test.lua
+++ tests/[linux]commandline_test.lua
@@ -1,18 +1,29 @@
+LIDE_PATH = (os.getenv 'LIDE_PATH')
+
 -- Run standard internal tests:
 io.stdout : write '[lide commandline] execution tests: '
 
-	assert(io.popen ('./lide.sh --test') :read '*a' == '[lide test] all ok.\n')
+	assert(io.popen ('./lide.sh --test') :read '*a':sub(1,19) == '[lide test] all ok.')
 
 io.stdout : write '\t[OK]\n'
 
 -- Test if luasql, lfs are on stable repo with search:
 io.stdout : write '[lide commandline] search tests: '
 	
-	searchline = io.popen ('./lide.sh search luasql') :read '*l'
+	searchline = io.popen ('./lide.sh search lanes') :read '*l'
+	print(searchline:sub (1, searchline : find '/' ))
 	assert( searchline:sub (1, searchline : find '/' ) == 'stable/')
-	searchline = io.popen ('./lide.sh search lfs') :read '*l'
+	searchline = io.popen ('./lide.sh search lanes') :read '*l'
 	assert( searchline:sub (1, searchline : find '/' ) == 'stable/')
 
 io.stdout : write '\t[OK]\n'
 
 --
+
+io.stdout : write '[lide commandline] package install tests: '
+
+	installline = io.popen ('./lide.sh install lanes') :read '*a'
+	
+    assert(type(io.open(LIDE_PATH .. '/libraries/linux/x64/clibs/lanes/core.so', 'rb')) == 'userdata')
+
+io.stdout : write '\t[OK]\n'
