d39e8dabc5ab3d5e186ae9eb97a856f01788b63a
H.D. Cano <thdkano@gmail.com>
2017-12-19 03:54:54 -0500

    Add x64 runtime libraries for MS Windows

--- lide.bat
+++ lide.bat
@@ -1,2 +1,6 @@
 @echo off 
-%LIDE_PATH%\bin\windows\x86\lua5.1.exe %LIDE_PATH%\lide.lua %1 %2 %3 %4 %5 %6 %7
+if %PROCESSOR_ARCHITECTURE%==x86 (
+  %LIDE_PATH%\bin\windows\x86\lua5.1.exe %LIDE_PATH%\lide.lua %1 %2 %3 %4 %5 %6 %7
+) else (
+  %LIDE_PATH%\bin\windows\x64\lua5.1.exe %LIDE_PATH%\lide.lua %1 %2 %3 %4 %5 %6 %7
+)
