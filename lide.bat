9eda556ea148125717b4cafd514476af07363799
H.D. Cano <thdkano@gmail.com>
2018-01-15 00:30:50 -0500

    Limit support for x86 binaries on Windows

--- lide.bat
+++ lide.bat
@@ -1,6 +1,4 @@
 @echo off 
-if %PROCESSOR_ARCHITECTURE%==x86 (
-  %LIDE_PATH%\bin\windows\x86\lua5.1.exe %LIDE_PATH%\lide.lua %1 %2 %3 %4 %5 %6 %7
-) else (
-  %LIDE_PATH%\bin\windows\x64\lua5.1.exe %LIDE_PATH%\lide.lua %1 %2 %3 %4 %5 %6 %7
-)
+setlocal 
+
+%LIDE_PATH%\bin\windows\x86\lua5.1.exe %LIDE_PATH%\lide.lua %1 %2 %3 %4 %5 %6 %7
