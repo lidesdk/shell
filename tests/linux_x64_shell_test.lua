e14fc2cb6d11b8b80270263f99923b33c747e90c
Hernan Dario Cano <dcanohdev@gmail.com>
2018-07-29 22:37:21 -0500

    Update tests

---
+++ tests/linux_x64_shell_test.lua
@@ -0,0 +1,29 @@
+local LIDE_PATH
+
+if os.getenv 'LIDE_PATH' then
+	LIDE_PATH = os.getenv 'LIDE_PATH'
+end
+
+-- Run standard internal tests:
+io.stdout : write '[lide shell] execution tests: '
+
+	assert(io.popen './lide.sh --test' :read '*a' == '[lide test] all ok.\n')
+
+io.stdout : write '\t[OK]\n'
+
+-- Test if luasql, lfs are on stable repo with search:
+io.stdout : write '[lide shell] package search: '
+	
+	 searchline = io.popen './lide.sh search luasql' :read '*l'
+	 assert( searchline:sub (1, searchline : find '/' ) == 'stable/')
+	 searchline = io.popen './lide.sh search lfs' :read '*l'
+	 assert( searchline:sub (1, searchline : find '/' ) == 'stable/')
+
+io.stdout : write '\t[OK]\n'
+
+-- Test installation of lfs (1.7.0 for linux):
+io.stdout : write '[lide shell] package install: '
+	install_package = io.popen (LIDE_PATH .. '/lide.sh install lfs 1.7.0') :read '*a'   
+	assert(type(io.open(LIDE_PATH .. '/libraries/linux/x64/clibs/lfs.so', 'rb')) == 'userdata')
+
+io.stdout : write '\t[OK]\n'
--- tests/windows_x86_shell_test.lua
+++ tests/windows_x86_shell_test.lua
@@ -1,18 +1,29 @@
+local LIDE_PATH
+
+if os.getenv 'LIDE_PATH' then
+	LIDE_PATH = os.getenv 'LIDE_PATH'
+end
+
 -- Run standard internal tests:
-io.stdout : write '[lide commandline] execution tests: '
+io.stdout : write '[lide shell] execution tests: '
 
 	assert(io.popen '.\\lide.bat --test' :read '*a' == '[lide test] all ok.\n')
 
 io.stdout : write '\t[OK]\n'
 
 -- Test if luasql, lfs are on stable repo with search:
---io.stdout : write '[lide commandline] search tests: '
+io.stdout : write '[lide shell] package search: '
 	
-	-- searchline = io.popen '.\\lide.bat search luasql' :read '*l'
-	-- assert( searchline:sub (1, searchline : find '/' ) == 'stable/')
-	-- searchline = io.popen '.\\lide.bat search lfs' :read '*l'
-	-- assert( searchline:sub (1, searchline : find '/' ) == 'stable/')
+	 searchline = io.popen '.\\lide.bat search luasql' :read '*l'
+	 assert( searchline:sub (1, searchline : find '/' ) == 'stable/')
+	 searchline = io.popen '.\\lide.bat search lfs' :read '*l'
+	 assert( searchline:sub (1, searchline : find '/' ) == 'stable/')
 
---io.stdout : write '\t[OK]\n'
+io.stdout : write '\t[OK]\n'
 
---
+-- Test installation of lfs (1.4.20 for windows):
+io.stdout : write '[lide shell] package install: '
+	install_package = io.popen (LIDE_PATH .. '/lide.bat install lfs 1.4.20') :read '*a'   
+	assert(type(io.open(LIDE_PATH .. '/libraries/windows/x86/clibs/lfs.dll', 'rb')) == 'userdata')
+
+io.stdout : write '\t[OK]\n'
